68078823c576476aebc4026fe5e9e180690cd005
Victor Buendia <victorbuendia03@gmail.com>
2023-05-10 12:46:52 -0300

    Criação do projeto e arquivos base

---
+++ src/completude/Completude.java
@@ -0,0 +1,18 @@
+package completude;
+
+public class Completude {
+	// estrutura para armazenar os dados (estrutura de dados chave - valor, ex: hash)
+	
+	// estrutura para armazenar os dados
+	
+	
+	
+	
+	// método para adicionar valores aos dados
+	
+	// método para verificar se um campo composto está completo
+	
+	// método para verificar se um campo composto está completo (recursivo)
+	
+	// método principal para cálculo da completude (geral)
+}
---
+++ src/completude/TestCompletude.java
@@ -0,0 +1,46 @@
+package completude;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+class TestCompletude {
+
+	@Before
+	void setUp() throws Exception {
+	}
+
+	// Calculo da completude de campos OR EXCLUSIVO
+	// Fazer a falsificação, duplicação e triangulação
+	// Finalizar parametrizando os testes
+	@Test
+	void test1() {
+		fail("Calculo da completude de campos OR EXCLUSIVO");
+	}
+	
+	// Calculo da completude de campos OR INCLUSIVO
+	// Fazer a falsificação, duplicação e triangulação
+	// Finalizar parametrizando os testes
+	@Test
+	void test2() {
+		fail("Calculo da completude de campos OR INCLUSIVO");
+	}
+
+	// Cálculo da completude de registros multi-campos
+	// Fazer a falsificação, duplicação e triangulação
+	// Finalizar parametrizando os testes
+	@Test
+	void test3() {
+		fail("Cálculo da completude de registros multi-campos");
+	}
+
+	// Testes de exceção
+	// Não são parametrizados
+	@Test
+	void test4() {
+		fail("Exceção");
+	}
+
+
+}
