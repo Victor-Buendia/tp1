ab7caf8cfacad7ce8ff1b4708cb3b8e5cfd3b740
Victor Buendia <victorbuendia03@gmail.com>
2023-05-23 20:14:34 -0300

    Stable: Test Case creation for multi fields
     Co-authored-by: Arthur-Gaudium <arthurmatos@gaudium.com.br>
     Co-authored-by: Klyssmann Oliveira <klyssmannoliveira@gmail.com>
     Co-authored-by: jvsdurso <jvsdurso@gmail.com>

---
+++ src/teste/TesteCompletudeFioCruz.java
@@ -0,0 +1,44 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import completude.Completude;
+import parser.JSONParser;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+@RunWith(Parameterized.class)
+public class TesteCompletudeFioCruz {
+    @Parameters
+    public static Collection<Object[]> getParameters() {
+    	String filePath = "src/json/extratoFioCruzCorrigido.json";
+    	Object[] testCases = JSONParser.parseJSONFile(filePath);
+    	
+        return Arrays.asList(new Object[][] {
+            { testCases[0], 79.68 },
+        });
+    }
+    
+    private Object[][] camposAninhados;
+    private double resultadoEsperado;
+    
+    public TesteCompletudeFioCruz(Object[][] camposAninhados, double resultadoEsperado) {
+        this.camposAninhados = camposAninhados;
+        this.resultadoEsperado = resultadoEsperado;
+    }
+    
+    @Test
+    public void testCompletudeOrInclusivo() {
+        Completude c = new Completude(camposAninhados);
+        c.imprimeCampos();
+        assertEquals(resultadoEsperado, c.calcularCompletudeMultiCampos(), 0.1);
+    }
+}
